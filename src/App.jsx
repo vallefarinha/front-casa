c14296d7defe7c4cc9e992320d2982e428294990
Gabriela Farinha <vallefarinha@gmail.com>
2024-04-03 09:53:36 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Router from "./configuration/Router.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar.jsx";
+
+const App = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar/>
+      <div className="flex-grow">
+        <Router />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
