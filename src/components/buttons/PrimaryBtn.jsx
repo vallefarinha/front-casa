5e6ef32441f29d52f01fa85b2d52ec4f449f08e2
NathaRuiz <nruizm98@gmail.com>
2024-04-04 11:13:37 +0200

    update font in PrimaryBtn

--- src/components/buttons/PrimaryBtn.jsx
+++ src/components/buttons/PrimaryBtn.jsx
@@ -6,7 +6,7 @@ const PrimaryBtn = ({data}) => {
     <>
       <a
         type="button"
-        className="text-LetterColor bg-tertiaryColor hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-tertiaryColor font-medium rounded-3xl text-sm px-6 py-2 text-center"
+        className="text-LetterColor bg-tertiaryColor hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-tertiaryColor font-bold shadow-md rounded-3xl text-sm px-6 py-2 text-center"
       >
         {data}
       </a>
