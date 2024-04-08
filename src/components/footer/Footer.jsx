c19a3ea495f4d52591841700a3cd28fd604fe645
diana <dialomt@gmail.com>
2024-04-08 13:41:28 +0200

    footer image

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 
+const ContactInfo = () => (
+  <div className="w-[474.61px] h-[91.59px]">
+    <div className="text-zinc-800 text-base font-normal font-poppinsRegular">
+      Avda. del profesor Pérez Pimentel,105 Barrio de la Guía<br/>33203 Gijón - Asturias
+    </div>
+    <div className="text-zinc-800 text-base font-normal font-poppinsRegular">
+      tlfn. 985 363 314
+    </div>
+    <div className="text-zinc-800 text-base font-normal font-poppinsRegular underline leading-7 tracking-tight">
+      casadeacogidaguia@hotmail.es
+    </div>
+  </div>
+);
+
 const Footer = () => (
   <div className="w-[1443px] h-[190px] relative">
-    <div className="w-[1442.51px] h-[190px] left-0 top-0 absolute">
-      <div className="w-[1442.51px] h-[190px] left-0 top-0 absolute bg-white" />
-      <div className="w-[407.71px] h-6 left-[977.70px] top-[19px] absolute text-rose-500 text-[22px] font-semibold font-poppinsBold">Información de contacto</div>
-      <div className="w-[23.04px] h-[25px] left-[448.78px] top-[74px] absolute" />
+    <div className="w-[1442.51px] h-[190px] left-0 top-0 absolute bg-white shadow-xl">
+      <div className="w-[407.71px] h-6 left-[931.62px] top-[19px] absolute text-rose-500 text-xl font-semibold font-poppinsBold">
+        Información de contacto
+      </div>
     </div>
-    <div className="w-[1442.51px] h-[190px] left-0 top-0 absolute shadow-xl">
-      <div className="w-[1442.51px] h-[190px] left-0 top-0 absolute bg-white" />
-      <div className="w-[407.71px] h-6 left-[931.62px] top-[19px] absolute text-rose-500 text-xl font-semibold font-poppinsBold">Información de contacto</div>
-    </div>
-    <img className="w-[188.31px] h-[137.38px] left-[18.03px] top-[26px] absolute" src="https://via.placeholder.com/188x137" />
+    <img className="w-[188.31px] h-[137.38px] left-[18.03px] top-[26px] absolute" src="src\assets\images\LogoSanta.svg" />
     <div className="w-[512.38px] h-[91.59px] left-[930.62px] top-[68px] absolute">
-      <div className="w-[474.61px] h-[31.18px] left-[37.77px] top-0 absolute text-zinc-800 text-base font-normal font-poppinsRegular">Avda. del profesor Pérez Pimentel,105 Barrio de la Guía<br/>33203 Gijón - Asturias</div>
-      <div className="w-[141.17px] h-[15.59px] left-[36.71px] top-[39.95px] absolute text-zinc-800 text-base font-normal font-poppinsRegular">tlfn. 985 363 314</div>
-      <div className="w-[292.07px] h-[27.28px] left-[35.66px] top-[64.31px] absolute text-zinc-800 text-base font-normal font-poppinsRegular underline leading-7 tracking-tight">casadeacogidaguia@hotmail.es</div>
-      <div className="w-[23.04px] h-[25px] left-0 top-0 absolute" />
+      <ContactInfo />
     </div>
   </div>
 );
 
 export default Footer;
+
