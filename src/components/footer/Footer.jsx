cf703a3be1ed9e2046d0e08b09e8f00bca87d4b5
diana <dialomt@gmail.com>
2024-04-08 15:30:36 +0200

    position ok

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import LogoNav from "../../assets/images/LogoSanta.svg";
 
 const ContactInfo = () => (
-  <div className="w-[473.79px] h-[91.59px]">
-    <div className="text-LetterColor text-base font-normal font-poppinsRegular">
-      Avda. del profesor Pérez Pimentel,105 Barrio de la Guía<br/>33203 Gijón - Asturias
-    </div>
-    <div className="text-LetterColor text-base font-normal font-poppinsRegular">
-      tlfn. 985 363 314
-    </div>
-    <a href="mailto:casadeacogidaguia@hotmail.es" className="text-LetterColor text-base font-normal font-poppinsRegular underline leading-7 tracking-tight">
+  <div className="text-LetterColor text-base font-normal font-poppinsRegular">
+    Avda. del profesor Pérez Pimentel,105 Barrio de la Guía<br/>33203 Gijón - Asturias
+    <br/>
+    tlfn. 985 363 314
+    <br/>
+    <a href="mailto:casadeacogidaguia@hotmail.es" className="underline leading-7 tracking-tight">
       casadeacogidaguia@hotmail.es
     </a>
   </div>
 );
 
 const Footer = () => (
-  <div className="w-[1440.49px] h-[190px] relative shadow-xl">
-    <div className="w-[1440px] h-[190px] left-0 top-0 absolute bg-white">
-      <div className="w-[407px] h-6 left-[930px] top-[19px] absolute text-primaryColor text-xl font-semibold font-poppinsBold">
+  <div className="bg-white w-full h-auto py-6 px-4 md:px-6 flex items-center justify-between shadow-xl">
+    <div className="flex items-center space-x-3">
+    <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src={LogoNav} className="h-16" alt="Logo" />
+    </Link>
+      
+    </div>
+    <div className="w-2/3 md:w-1/2">
+      <div className="text-primaryColor text-xl font-semibold font-poppinsBold">
         Información de contacto
       </div>
-    </div>
-    <img className="w-[187.99px] h-[137.38px] left-[18px] top-[26px] absolute" src="src\assets\images\LogoSanta.svg" />
-    <div className="w-[511.49px] h-[91.59px] left-[929px] top-[68px] absolute">
       <ContactInfo />
     </div>
   </div>
