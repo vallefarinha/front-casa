bd6217dbc4d38f460f15f6831a977813a64d5839
diana <dialomt@gmail.com>
2024-04-08 20:19:03 +0200

    shadow line

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,28 +3,26 @@ import { Link } from 'react-router-dom';
 import LogoNav from "../../assets/images/LogoSanta.svg";
 
 const ContactInfo = () => (
-  <div className="text-LetterColor text-base font-normal font-poppinsRegular">
-    Avda. del profesor Pérez Pimentel,105 Barrio de la Guía<br/>33203 Gijón - Asturias
-    <br/>
-    tlfn. 985 363 314
-    <br/>
-    <a href="mailto:casadeacogidaguia@hotmail.es" className="underline leading-7 tracking-tight">
+  <div className="text-LetterColor text-sm md:text-sm font-normal font-poppinsRegular leading-none space-y-0">
+    Avda. del profesor Pérez Pimentel,105 Barrio de la Guía<br/>
+    33203 Gijón - Asturias<br/>
+    <div className="leading-none">tlfn. 985 363 314</div>
+    <div className="leading-none"><a href="mailto:casadeacogidaguia@hotmail.es" className="underline">
       casadeacogidaguia@hotmail.es
-    </a>
+    </a> </div>
   </div>
 );
 
 const Footer = () => (
-  <div className="bg-white w-full h-auto py-6 px-4 md:px-6 flex items-center justify-between shadow-xl">
-    <div className="flex items-center space-x-3">
-    <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+  <div className="bg-white w-full h-24 py-6 px-4 md:px-6 flex items-center justify-between shadow-xl border-t-2 border-gray-200">
+    <div className="flex items-center space-x-3" style={{marginLeft: '9%'}}>
+      <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
         <img src={LogoNav} className="h-16" alt="Logo" />
-    </Link>
-      
+      </Link>
     </div>
-    <div className="w-2/3 md:w-1/2 flex flex-col items-end">
+    <div className="w-2/3 md:w-1/2 flex flex-col items-end"style={{marginRight: '5%'}}>
       <div className="-mr-2">
-        <div className="text-primaryColor text-xl font-semibold font-poppinsBold">
+        <div className="text-primaryColor text-lg font-semibold font-poppinsBold leading-none">
           Información de contacto
         </div>
         <ContactInfo />
