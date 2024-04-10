3061ca9cc3f3ea8cc10a8a71bf2bb275ee999ce3
diana <dialomt@gmail.com>
2024-04-10 13:12:09 +0200

    title agree home

--- src/components/cards/WhatWeDoCard/OurWorkCardWithIcon.jsx
+++ src/components/cards/WhatWeDoCard/OurWorkCardWithIcon.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const OurWorkCardWithIcon = ({ imgSrc, altText, cardText }) => {
   return (
     <div>
-      <div className="flex mt-20 flex-col items-center pb-10">
+      <div className="flex mt-20 flex-col items-center pb-10 m-4">
         <img
           class="w-14 h-14 mb-3 rounded-full"
           src={imgSrc}
           alt={altText}
         />
-        <span className="text-x font-poppinsRegular text-LetterColor">
+        <span className="text-x font-poppinsRegular text-LetterColor w-[200px] text-center">
           {cardText}
         </span>
       </div>
