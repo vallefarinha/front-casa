7f466ca4c3eaf02a202a9cb360e31d71317c1774
diana <dialomt@gmail.com>
2024-04-10 17:49:13 +0200

    finish page thanks

--- src/components/texts/RegularText30.jsx
+++ src/components/texts/RegularText30.jsx
@@ -1,6 +1,6 @@
 function RegularText30({ text, textAlign, size }) {
     return (
-        <h1 className={`my-4 mb-6 font-poppinsRegular w-[85%] text-${size} text-${textAlign}`}>
+        <h1 className={`my-4 mb-6 font-poppinsRegular w-full text-${size} text-${textAlign}`}>
             <span className="text-LetterColor">{text}</span>
         </h1>
     );
--- src/pages/ThanksForSignIn.jsx
+++ src/pages/ThanksForSignIn.jsx
@@ -4,15 +4,15 @@ import Title from "../components/title/Title"
 
 const ThanksForSignIn = () => {
   return (
-    <>
-      <div className="w-full">
+    <div className="w-[85%] mx-auto">
+      <div className="w-full mb-5">
         <Title 
             wordBlue="Gracias por"
             wordPink="suscribirte"
             textAlign="text-center"
         />
-        </div>
-      <div className="w-full">
+      </div>
+      <div className="w-full mt-6">
         <RegularText30 
             text="Tus datos se han guardado correctamente!"
             textAlign="center"
@@ -29,8 +29,9 @@ const ThanksForSignIn = () => {
       <div className='flex justify-center'>
       <img src="src\assets\icons\heartSun.svg" alt="heart Sun" className="w-1/4" />
       </div>
-    </>
+    </div>
   )
 }
 
 export default ThanksForSignIn
+
