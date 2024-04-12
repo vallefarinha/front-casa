b9e7dd0aa6220773d7fcaa0ae0de4055f5e6e87e
diana <dialomt@gmail.com>
2024-04-12 14:27:38 +0200

    ALIGN ELEMENTS

--- src/components/form/TitleForm.jsx
+++ src/components/form/TitleForm.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-function TitleForm({ wordBlue, wordPink, textAlign }) {
+function TitleForm({ wordBlue, wordPink }) {
   return (
-    <div className="w-full max-w-[85%] h-[186px] relative bg-rose-50 rounded-[20px] shadow">
-      <div className="w-[825px] h-[91px] mx-auto my-4">
-        <h1 className={`my-4 ${textAlign} text-6xl font-poppinsMedium w-full`}>
-          <span className="text-cyan-950">{wordBlue}</span>{" "}
-          <span className="text-rose-500 font-poppinsExtraBold">
-            {wordPink}
-          </span>
-        </h1>
-      </div>
+    <div style={{width: '900px'}} className="h-[186px] relative bg-rose-50 rounded-[20px] shadow flex items-center justify-center">
+      <h1 className="text-6xl font-poppinsMedium">
+        <span className="text-cyan-950">{wordBlue}</span>{" "}
+        <span className="text-rose-500 font-poppinsExtraBold">
+          {wordPink}
+        </span>
+      </h1>
     </div>
   );
 }
