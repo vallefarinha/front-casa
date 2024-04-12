b47070b936dffa5e0a21d29626cffa181ce9b5c4
diana <dialomt@gmail.com>
2024-04-12 14:04:17 +0200

    donation open

--- src/components/form/DonationOption.jsx
+++ src/components/form/DonationOption.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import BoxSelect from "../form/BoxSelect";
 import TitleForm from "../form/TitleForm";
 
 const DonationOption = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex flex-col items-center w-[85%] mx-auto mt-6">
-      <TitleForm wordBlue="¿Deseas recibir certificado de" wordPink="donación?" textAlign="center" />
-      <div className="flex space-x-4 mt-8">
-            <BoxSelect amount="SÍ" customStyle={{width: '150px', height: '100px'}} textSize="2xl" />
-            <BoxSelect amount="NO" customStyle={{width: '150px', height: '100px'}} textSize="2xl" />
-      </div>
+      <button className="bg-transparent border-none p-0 m-0" onClick={() => setIsOpen(!isOpen)}>
+        <TitleForm wordBlue="¿Deseas recibir certificado de" wordPink="donación?" textAlign="center" />
+      </button>
+      {isOpen && (
+        <div className="flex space-x-4 mt-8">
+          <BoxSelect amount="SÍ" customStyle={{width: '150px', height: '100px'}} textSize="3xl" />
+          <BoxSelect amount="NO" customStyle={{width: '150px', height: '100px'}} textSize="3xl" />
+        </div>
+      )}
     </div>
   );
 };
 
 export default DonationOption;
+
