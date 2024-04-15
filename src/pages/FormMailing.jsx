01f0ccc708737468540720d95f96d7926fff5bfc
maicaocaa <maria.caocaa@gmail.com>
2024-04-15 15:59:49 +0200

    configure env const

--- src/pages/FormMailing.jsx
+++ src/pages/FormMailing.jsx
@@ -2,7 +2,20 @@ import React, {useRef} from 'react';
 import emailjs from '@emailjs/browser';
 
 
-
+const BoxBlue = ({ titleBox, squareBlue, type }) => {
+    return (
+      <div className="flex flex-col mb-6 w-full px-2">
+        <label htmlFor={titleBox} className="text-LetterColor font-poppinsBold text-xl mb-2">{titleBox}</label>
+        <input
+          type={type}
+          id={titleBox}
+          placeholder={squareBlue}
+          className="border-2 border-quarteryColor rounded-lg py-4 px-6 shadow-xl focus:outline-none bg-white"
+          required
+        />
+      </div>
+    );
+  };
 
 
 const FormMailing = () => {
@@ -13,16 +26,14 @@ const FormMailing = () => {
      event.preventDefault();
      console.log(refForm.current);
      //defino constantes
-     const serviceID = "service_4jji07i";
-     const templateID = "template_duo14qk";
+     const serviceID = import.meta.env.VITE_REACT_APP_SERVICE_ID;
+     const templateID = import.meta.env.VITE_REACT_APP_TEMPLATE_ID;
      //3 parametro
-     const apikey ="bIhYCe7gWQU3IfgdW";
+     const apikey =import.meta.env.VITE_REACT_APP_PUBLIC_KEY;
 
      emailjs.sendForm(serviceID, templateID,refForm.current, apikey)
      .then(result=>console.log(result.text))
      .catch(error=>console.error(error))
-
-
 
    }
 
@@ -31,9 +42,12 @@ const FormMailing = () => {
 
     <div>
         <form ref={refForm} action="" onSubmit={handleSubmit}>
+
             <fieldset>
-            <label   htmlFor="name">Name</label>
-            <input placeholder='nombre' name="name" type="text" id="name" required />
+                <label   htmlFor="name">Name</label>
+                <input placeholder='nombre' name="name" type="text" id="name" required />
+
+            
             </fieldset>
 
             <fieldset>
@@ -46,10 +60,34 @@ const FormMailing = () => {
             <textarea name="message" maxLength="500" placeholder='Insert your message' required id="message" cols="30" rows="10"></textarea>
             </fieldset>
 
-            <button>Send</button>
+            
+
+       
+    
+{/*        
+            <div className="flex items-center justify-center mt-6">
+                <div className="relative w-1/2 h-1/2 flex flex-wrap m-10" style={{backgroundColor: 'rgba(17, 138, 178, 0.6)', borderRadius: '20px'}}>
+                        <div className="w-full sm:w-1/2 p-4">
+                            <BoxBlue titleBox="Nombre *" squareBlue="Ingrese su nombre" />
+                            <BoxBlue titleBox="DNI/NIE *" squareBlue="Ingrese su DNI/NIE" />
+                            <BoxBlue titleBox="Email *" squareBlue="Ingrese su correo electrónico" />
+                            <BoxBlue titleBox="Ciudad" squareBlue="Ingrese su ciudad" />
+                            <BoxBlue titleBox="Código Postal" squareBlue="Ingrese su código postal" />
+                        </div>
+                
+                        <div className="w-full sm:w-1/2 p-4">
+                            <BoxBlue titleBox="Apellidos *" squareBlue="Ingrese sus apellidos" />
+                            <BoxBlue titleBox="Teléfono" squareBlue="Ingrese su número de teléfono" />
+                            <BoxBlue titleBox="Dirección" squareBlue="Ingrese su dirección" />
+                            <BoxBlue titleBox="Provincia" squareBlue="Ingrese su provincia" />
+                            <BoxBlue titleBox="País" squareBlue="Ingrese su país" />
+                        </div>
+                </div>
+            </div> */}
+
+        <button>Send</button>
 
         </form>
-    
     
 
 
