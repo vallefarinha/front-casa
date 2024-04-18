01c0ec60bd98883c9b99831285416b75f4e38f5a
diana <dialomt@gmail.com>
2024-04-18 16:13:52 +0200

    bankblue pattern

--- src/components/form/BankBlue.jsx
+++ src/components/form/BankBlue.jsx
@@ -15,7 +15,7 @@ const BankBlue = () => {
         <div className="relative w-1/2 h-1/2 flex flex-wrap mt-5 mb-2" style={{backgroundColor: 'rgba(17, 138, 178, 0.6)', borderRadius: '20px'}}>
           <div className="w-full sm:w-1/2 p-4">
             <BoxBlueAtom titleBox="Titular" name="titular" squareBlue="Ingrese el titular de la cuenta" type="text" />
-            <BoxBlueAtom titleBox="IBAN" name="IBAN" squareBlue="Ingrese su IBAN" type="text" />
+            <BoxBlueAtom titleBox="IBAN" name="IBAN" squareBlue="Ingrese su IBAN" type="text" pattern="^ES\d{22}$" />
           </div>
           <img src={rainbowpink} alt="rainbowpink" className="w-[65px] h-[69px] absolute top-[52%] left-[68%] transform -translate-x-1/2 -translate-y-1/2 md:w-[178px] md:h-[170px] md:left-[78%] md:top-[42%]" />
         </div>
