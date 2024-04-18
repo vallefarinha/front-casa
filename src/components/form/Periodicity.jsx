d06189ed69995280b72fdbf106bc4e4caa6e94f3
diana <dialomt@gmail.com>
2024-04-18 12:53:08 +0200

    all button function sendmail

--- src/components/form/Periodicity.jsx
+++ src/components/form/Periodicity.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import BoxSelect from "../form/BoxSelect";
 import TitleForm from "../form/TitleForm";
 import heart from "../../assets/icons/heart.svg";
@@ -6,6 +6,12 @@ import heart from "../../assets/icons/heart.svg";
 
 const Periodicity = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedAmount, setSelectedAmount] = useState("");
+  const inputRef = useRef();
+
+  const handleSelectClick = (amount) => {
+    setSelectedAmount(amount);
+  };
 
   return (
     <div className="flex flex-col items-center w-[85%] mx-auto mt-6">
@@ -15,10 +21,10 @@ const Periodicity = () => {
       {isOpen && (
         <div className="flex flex-wrap justify-center space-x-4 mt-8 relative">
         <img src={heart} alt="heart"  className="w-[55px] h-[59px] absolute top-[40%] left-[10%] transform -translate-x-1/2 -translate-y-1/2 md:w-[108px] md:h-[100px] md:left-[111%] md:top-[42%]" />
-          <BoxSelect amount="MENSUAL" customStyle={{width: '100px', height: '50px'}} textSize="lg" className="mb-4 md:mb-0" />
-          <BoxSelect amount="TRIMESTRAL" customStyle={{width: '100px', height: '50px'}} textSize="md" className="mb-4 md:mb-0" />
-          <BoxSelect amount="SEMESTRAL" customStyle={{width: '100px', height: '50px'}} textSize="md" className="mb-4 md:mb-0" />
-          <BoxSelect amount="ANUAL" customStyle={{width: '100px', height: '50px'}} textSize="lg" className="mb-4 md:mb-0" />
+          <BoxSelect amount="MENSUAL" customStyle={{width: '100px', height: '50px'}} textSize="lg" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick}/>
+          <BoxSelect amount="TRIMESTRAL" customStyle={{width: '100px', height: '50px'}} textSize="md" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick}/>
+          <BoxSelect amount="SEMESTRAL" customStyle={{width: '100px', height: '50px'}} textSize="md" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick}/>
+          <BoxSelect amount="ANUAL" customStyle={{width: '100px', height: '50px'}} textSize="lg" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick} />
         </div>
       )}
     </div>
