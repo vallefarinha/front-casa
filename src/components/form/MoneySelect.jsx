0374e0b091ac64899f857b690553dc2f313e5b94
diana <dialomt@gmail.com>
2024-04-18 15:34:42 +0200

    moneyselect only one button clic

--- src/components/form/MoneySelect.jsx
+++ src/components/form/MoneySelect.jsx
@@ -16,28 +16,24 @@ const MoneySelect = () => {
     }
   };
   
-  const handleInputChange = (event) => {
-    setSelectedAmount(event.target.value);
-  };
-
-    return (
-      <div className="flex flex-col items-center w-[85%] mx-auto mt-6">
-        <button className="bg-transparent border-none p-0 m-0" onClick={() => setIsOpen(!isOpen)}>
-          <TitleForm wordBlue="¿Cuánto quieres" wordPink="aportar?" textAlign="center" />
-        </button>
-        {isOpen && (
-          <div className="flex flex-wrap justify-center space-x-4 mt-8 relative">
-            <img src={kinMoney} alt="Kin Money" className="w-[65px] h-[69px] absolute top-[40%] left-[8%] transform -translate-x-1/2 -translate-y-1/2 md:w-[120px] md:h-[100px] md:left-[1%] md:top-[42%]" />
-            <img src={jigsawMoney} alt="jigsaw" className="w-[45px] h-[49px] absolute top-[10%] left-[90%] transform -translate-x-1/2 -translate-y-1/2 md:w-[98px] md:h-[70px] md:left-[112%] md:top-[42%]" />
-            <BoxSelect amount="20 €" customStyle={{width: '100px', height: '50px'}} textSize="2xl" className="mb-4 md:mb-0 z-20" inputRef={inputRef} onClick={handleSelectClick} />
-            <BoxSelect amount="40 €" customStyle={{width: '100px', height: '50px'}} textSize="2xl" className="mb-4 md:mb-0 z-20" inputRef={inputRef} onClick={handleSelectClick} />
-            <BoxSelect amount="80 €" customStyle={{width: '100px', height: '50px'}} textSize="2xl" className="mb-4 md:mb-0 z-20" inputRef={inputRef} onClick={handleSelectClick} />
-            <BoxSelect amount="otra cantidad" customStyle={{width: '100px', height: '50px'}} textSize="xl" className="mb-4 md:mb-0 z-20" inputRef={inputRef} onClick={handleSelectClick} />
-            <input type="text" ref={inputRef} name="selectedAmountMoney" value={selectedAmountMoney} onChange={handleInputChange} style={{ display: 'none' }} />
-         </div>
-        )}
-      </div>
-    );
+  return (
+    <div className="flex flex-col items-center w-[85%] mx-auto mt-6">
+      <button className="bg-transparent border-none p-0 m-0" onClick={() => setIsOpen(!isOpen)}>
+        <TitleForm wordBlue="¿Cuánto quieres" wordPink="aportar?" textAlign="center" />
+      </button>
+      {isOpen && (
+        <div className="flex flex-wrap justify-center space-x-4 mt-8 relative">
+          <img src={kinMoney} alt="Kin Money" className="w-[65px] h-[69px] absolute top-[40%] left-[8%] transform -translate-x-1/2 -translate-y-1/2 md:w-[120px] md:h-[100px] md:left-[1%] md:top-[42%]" />
+          <img src={jigsawMoney} alt="jigsaw" className="w-[45px] h-[49px] absolute top-[10%] left-[90%] transform -translate-x-1/2 -translate-y-1/2 md:w-[98px] md:h-[70px] md:left-[112%] md:top-[42%]" />
+          <BoxSelect amount="20 €" customStyle={{width: '100px', height: '50px'}} textSize="2xl" className="mb-4 md:mb-0 z-20" inputRef={inputRef} onClick={handleSelectClick} isSelected={selectedAmountMoney === "20 €"} />
+          <BoxSelect amount="40 €" customStyle={{width: '100px', height: '50px'}} textSize="2xl" className="mb-4 md:mb-0 z-20" inputRef={inputRef} onClick={handleSelectClick} isSelected={selectedAmountMoney === "40 €"} />
+          <BoxSelect amount="80 €" customStyle={{width: '100px', height: '50px'}} textSize="2xl" className="mb-4 md:mb-0 z-20" inputRef={inputRef} onClick={handleSelectClick} isSelected={selectedAmountMoney === "80 €"} />
+          <BoxSelect amount="otra cantidad" customStyle={{width: '100px', height: '50px'}} textSize="xl" className="mb-4 md:mb-0 z-20" inputRef={inputRef} onClick={handleSelectClick} isSelected={selectedAmountMoney === "otra cantidad"} />
+          <input type="text" ref={inputRef} name="selectedAmountMoney" value={selectedAmountMoney} style={{ display: 'none' }} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MoneySelect;
