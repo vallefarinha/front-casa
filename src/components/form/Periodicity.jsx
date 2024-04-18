335985f2bce3b48999f23679260167d868d52063
diana <dialomt@gmail.com>
2024-04-18 15:41:13 +0200

    periodicuty only one button clic

--- src/components/form/Periodicity.jsx
+++ src/components/form/Periodicity.jsx
@@ -3,7 +3,6 @@ import BoxSelect from "../form/BoxSelect";
 import TitleForm from "../form/TitleForm";
 import heart from "../../assets/icons/heart.svg";
 
-
 const Periodicity = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedPeriodicity, setSelectedAmount] = useState("");
@@ -15,10 +14,6 @@ const Periodicity = () => {
       inputRef.current.value = amount;
     }
   };
-  
-  const handleInputChange = (event) => {
-    setSelectedAmount(event.target.value);
-  };
 
   return (
     <div className="flex flex-col items-center w-[85%] mx-auto mt-6">
@@ -28,12 +23,11 @@ const Periodicity = () => {
       {isOpen && (
         <div className="flex flex-wrap justify-center space-x-4 mt-8 relative">
         <img src={heart} alt="heart"  className="w-[55px] h-[59px] absolute top-[40%] left-[10%] transform -translate-x-1/2 -translate-y-1/2 md:w-[108px] md:h-[100px] md:left-[111%] md:top-[42%]" />
-          <BoxSelect amount="MENSUAL" customStyle={{width: '100px', height: '50px'}} textSize="lg" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick}/>
-          <BoxSelect amount="TRIMESTRAL" customStyle={{width: '100px', height: '50px'}} textSize="md" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick}/>
-          <BoxSelect amount="SEMESTRAL" customStyle={{width: '100px', height: '50px'}} textSize="md" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick}/>
-          <BoxSelect amount="ANUAL" customStyle={{width: '100px', height: '50px'}} textSize="lg" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick} />
-          <input type="text" ref={inputRef} name="selectedPeriodicity" value={selectedPeriodicity} onChange={handleInputChange} style={{ display: 'none' }} />
-
+          <BoxSelect amount="MENSUAL" customStyle={{width: '100px', height: '50px'}} textSize="lg" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick} isSelected={selectedPeriodicity === "MENSUAL"}/>
+          <BoxSelect amount="TRIMESTRAL" customStyle={{width: '100px', height: '50px'}} textSize="md" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick} isSelected={selectedPeriodicity === "TRIMESTRAL"}/>
+          <BoxSelect amount="SEMESTRAL" customStyle={{width: '100px', height: '50px'}} textSize="md" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick} isSelected={selectedPeriodicity === "SEMESTRAL"}/>
+          <BoxSelect amount="ANUAL" customStyle={{width: '100px', height: '50px'}} textSize="lg" className="mb-4 md:mb-0" inputRef={inputRef} onClick={handleSelectClick} isSelected={selectedPeriodicity === "ANUAL"}/>
+          <input type="text" ref={inputRef} name="selectedPeriodicity" value={selectedPeriodicity} style={{ display: 'none' }} />
         </div>
       )}
     </div>
