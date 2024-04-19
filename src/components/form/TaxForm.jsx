b582f12a0c971a3f0cba481848031fc69e9c738e
maicaocaaa <maria.caocaa@gmail.com>
2024-04-19 23:36:57 +0200

    eliminate stiles inline

--- src/components/form/TaxForm.jsx
+++ src/components/form/TaxForm.jsx
@@ -3,8 +3,8 @@ import BoxBlue from "./BoxBlueAtom";
 
 const TaxForm = () => {
   return (
-    <div className="flex items-center justify-center mt-[-10px]">
-    <div className="relative w-auto h-1/2 flex flex-wrap mt-5 mb-1" style={{backgroundColor: 'rgba(17, 138, 178, 0.6)', borderRadius: '20px'}}>
+    <div className="flex items-center justify-center">
+    <div className="relative w-auto h-1/2 flex flex-wrap mt-5 mb-1 bg-bgColorForm" style={{borderRadius: '20px'}}>
       
         <div className="w-full sm:w-1/2 p-4">
           <BoxBlue titleBox="Nombre *" name="name" squareBlue="Ingrese su nombre" type="text" />
--- src/components/form/formBlue.jsx
+++ src/components/form/formBlue.jsx
@@ -7,7 +7,7 @@ const FormBlue = () => {
 
   return (
     <div className="flex items-center justify-center mt-[-10px]">
-      <div className="relative w-auto h-1/2 flex flex-wrap mt-5 mb-1" style={{backgroundColor: 'rgba(17, 138, 178, 0.6)', borderRadius: '20px'}}>
+      <div className="relative w-auto h-1/2 flex flex-wrap mt-5 mb-1 bg-bgColorForm rounded-3xl" >
         
           <div className="w-full sm:w-1/2 p-4">
             <BoxBlue titleBox="Nombre *" name="name" squareBlue="Ingrese su nombre" type="text" />
