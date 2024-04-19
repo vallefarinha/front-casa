f11a44bea3572faecdc4ac6c26500a92e230b317
maicaocaa <maria.caocaa@gmail.com>
2024-04-19 11:19:16 +0200

    add tax text

---
+++ src/components/form/Tax.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TaxText from '../texts/TaxText'
+
+const Tax = () => {
+  return (
+    <div>Tax
+        <TaxText/>
+    </div>
+  )
+}
+
+export default Tax
--- src/pages/PartnerForm.jsx
+++ src/pages/PartnerForm.jsx
@@ -6,6 +6,7 @@ import TitleAllForm from "../components/form/TitleAllForm";
 import DonationOption from "../components/form/DonationOption";
 import SendButtonForm from "../components/form/SendButtonForm";
 import Legislation from "../components/form/Legislation"
+import Tax from "../components/form/Tax";
 
 function PartnerForm() {
   return (
@@ -23,7 +24,9 @@ function PartnerForm() {
         <DonationOption />
         <SendButtonForm />
       </section>
+      <Tax/>
       <Legislation/>
+      
     </div>
   );
 }
