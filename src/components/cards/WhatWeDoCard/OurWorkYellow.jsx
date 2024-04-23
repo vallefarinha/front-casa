1a714f7c7ff48dabe12cd0774c87673c7d0d44e6
maicaocaa <maria.caocaa@gmail.com>
2024-04-23 14:52:15 +0200

    fix src image fromt home

--- src/components/cards/WhatWeDoCard/OurWorkYellow.jsx
+++ src/components/cards/WhatWeDoCard/OurWorkYellow.jsx
@@ -2,49 +2,60 @@ import React from 'react';
 import OurWorkCardWithIcon from './OurWorkCardWithIcon';
 import Title from '../../title/Title';
 import OurWorkText from '../../texts/OurWorkText';
+import leftbubbles from '../../../assets/icons/leftBubbles.svg';
+import rightbubbles from '../../../assets/icons/rightBubbles.svg';
+import homeyellowicon from '../../../assets/icons/homeyellowicon.svg';
+import iconYellowHeart from '../../../assets/icons/iconYellowHeart.svg';
+import iconYellowPin from '../../../assets/icons/iconYellowPin.svg';
+import iconYellowLight from '../../../assets/icons/iconYellowLight.svg';
+import iconYellowSmiley from '../../../assets/icons/iconYellowSmiley.svg';
+import iconYellowCup from '../../../assets/icons/iconYellowCup.svg';
+
+
+
 
 const OurWorkYellow = () => {
   return (
     <div className="w-full">
       <section className="flex w-[90%] md:w-[95%] mx-auto justify-center items-center">
-        <img src="src\assets\icons\leftBubbles.svg" alt="left bubbles" className='w-1/4'/>
+        <img src={leftbubbles} alt="left bubbles" className='w-1/4'/>
         <Title
           wordBlue="Nuestro"
           wordPink="trabajo"
           textAlign="text-center"
         />
-        <img src="src\assets\icons\rightBubbles.svg" alt="right bubbles" className='w-1/4'/>
+        <img src={rightbubbles} alt="right bubbles" className='w-1/4'/>
       </section>
       <OurWorkText />
       <div className="flex flex-col items-center justify-center w-[90%] md:w-[80%] mx-auto">
         <div className="grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 mb-2"> 
           <OurWorkCardWithIcon
-            imgSrc="src\assets\icons\homeyellowicon.svg"
+            imgSrc={homeyellowicon}
             altText="yellow card"
             cardText="Desarrollo De Valores Humanos"
           />
           <OurWorkCardWithIcon
-            imgSrc="src\assets\icons\iconYellowHeart.svg"
+            imgSrc={iconYellowHeart}
             altText="yellow heart"
             cardText="Capacitación en Habilidades Maternales"
           />
           <OurWorkCardWithIcon
-            imgSrc="src\assets\icons\iconYellowPin.svg"
+            imgSrc={iconYellowPin}
             altText="yellow pin"
             cardText="Fomento de la Responsabilidad"
           />
           <OurWorkCardWithIcon
-            imgSrc="src\assets\icons\iconYellowLight.svg"
+            imgSrc={iconYellowLight}
             altText="yellow light"
             cardText="Descubrimiento de Potencialidades"
           />
           <OurWorkCardWithIcon
-            imgSrc="src\assets\icons\iconYellowSmiley.svg"
+            imgSrc={iconYellowSmiley}
             altText="yellow smile"
             cardText="Acompañamiento Emocional"
           />
           <OurWorkCardWithIcon
-            imgSrc="src\assets\icons\iconYellowCup.svg"
+            imgSrc={iconYellowCup}
             altText="yellow cup"
             cardText="Mejora de la Calidad de Vida"
           />
